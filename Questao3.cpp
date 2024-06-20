a0f4a6f44da1395b7de30fc69d74c8dc238e8852
jonas07br <jonasrafa07@gmail.com>
2024-06-20 01:44:31 -0300

    update

--- Questao3.cpp
+++ Questao3.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <random>
-#include <vector>
 #include <chrono>
 
 using namespace std;
@@ -9,45 +8,34 @@ Implemente um dos algoritmos de complexidade O(n²): escolha entre o
  algoritmo insertion sort e o selection sort.
  */
 
+void selectionSort(int lista[],int tam){
+    int i,j,min_value;
+    for(int i = 0; i<tam-1;i++){
+       
+        min_value=i;
+        for(j=i+1;j<tam;j++){
+            if(lista[j]<lista[min_value]){
+                min_value=j;
+            }
+        }
+
+        if(min_value!=i){
+            swap(lista[min_value],lista[i]);
+        }
+    }
+    for(int s =0 ;s<tam;s++){
+        cout << lista[s] << "-";
+    }
+
+}
+
 int main(){
     int x =0;
     cin >> x;
-    vector<int> lista(x);
-    cout << "lista gerada aleatoriamente" << endl;
+    int lista[x];
     for(int i =0; i<x;i++){
         lista[i] = rand()%x +1;
         
     }
-    cout << endl;
-
-    auto inicio =chrono::high_resolution_clock::now();
-
-    for(int z =0 ;z<x-1;z++){
-        int aux=0;
-        if(lista[z]>lista[z+1]){
-            aux=lista[z+1];
-            lista[z+1]=lista[z];
-            lista[z]=aux;
-        }
-        int w =z-1;
-        while(w>=0){
-            if(aux<lista[w]){
-                lista[w+1]=lista[w];
-                lista[w]=aux;
-            }
-            else{
-                break;
-            }
-            w=w-1;
-        }
-    }
-    cout << "lista ordenada com insertion sort" << endl;
-    for(int i =0; i<x;i++){
-    }
-    cout << endl;
-
-    auto tempo = chrono::high_resolution_clock::now() - inicio;
-    long long microseconds = chrono::duration_cast<std::chrono::milliseconds>(tempo).count();
-    cout <<"Tempo de execucaoo em milisegundos: "<< microseconds << endl;
-    
+    selectionSort(lista,x);
 }
